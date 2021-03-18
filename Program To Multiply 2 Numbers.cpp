043f3ca8aa90faf31095a2deb414f59230ee1c6f
Mehank <70057473+A-RAT-EXPLOIT@users.noreply.github.com>
2021-03-18 23:11:51 +0530

    Create Program To Multiply 2 Numbers.cpp

---
+++ Program To Multiply 2 Numbers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double num1, num2, ans;
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+    ans = num1 * num2;  
+    cout << "Product = " << ans;    
+    return 0;
+}

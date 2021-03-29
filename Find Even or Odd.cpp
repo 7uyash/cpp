3f28edc9566f0614e344806997f6df42ed79614c
Mehank <70057473+M3hank@users.noreply.github.com>
2021-03-29 22:17:52 +0530

    Create Find Even or Odd.cpp

---
+++ Find Even or Odd.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "ENTER A NUMBER: ";
+    cin >> n;
+    if ( n % 2 == 0)
+   cout << n << " IS A EVEN NUMBER.";
+    else
+    cout << n << " IS A ODD NUMBER.";
+    return 0;
+}
